f053c176be98200cb5e02be6fdd0f3f6f56e05d5
Campostrini <stefano.campostrini@outlook.it>
2021-04-08 16:34:09 +0200

    bufix: make proper videos. Loading from checkpoint was the issue.

--- dwd_dl/callbacks.py
+++ dwd_dl/callbacks.py
@@ -3,18 +3,13 @@ from pytorch_lightning.callbacks import ModelCheckpoint, Callback
 from dwd_dl import cfg
 
 
-class CreateVideo(Callback):
-    def __init__(self):
-        super().__init__()
-
-
 class CallbacksList:
     def __init__(self, experiment_timestamp_str):
         val_loss_checkpoint = ModelCheckpoint(
             monitor='valid_loss',
             dirpath=cfg.CFG.create_checkpoint_dir(experiment_timestamp_str),
-            filename=f'{experiment_timestamp_str}' + '-{epoch:02d}-{val_loss:.2f}',
-            save_top_k=1,
+            filename=f'{experiment_timestamp_str}' + '-{epoch:02d}-{valid_loss:.2f}',
+            save_top_k=2,
             mode='min',
         )
 

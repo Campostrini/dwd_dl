1c472be3972a47fcb65fc5b5354511e4143d7d3a
Campostrini <stefano.campostrini@outlook.it>
2022-01-20 09:55:00 +0100

    Fix: parsers fixed

--- dwd_dl/cli.py
+++ dwd_dl/cli.py
@@ -10,39 +10,44 @@ class RadolanParser(argparse.ArgumentParser):
         super().__init__(
             description="Training U-Net model for segmentation of RADOLAN precipitation images"
         )
-        self.add_argument(
+        self.add_arguments(self)
+
+    @staticmethod
+    def add_arguments(parser):
+        parser.add_argument(
             "--device",
             type=str,
             default="cuda:0",
             help="device for training (default: cuda:0)",
         )
-        self.add_argument(
+        parser.add_argument(
             "--workers",
             type=int,
             default=4,
             help="number of workers for data loading (default: 4)",
         )
-        self.add_argument(
+        parser.add_argument(
             "--weights", type=str, default="./weights", help="folder to save weights"
         )
-        self.add_argument(
+        parser.add_argument(
             "--logs", type=str, default="./logs", help="folder to save logs"
         )
-        self.add_argument(
+        parser.add_argument(
             "--images", type=str, default="../Radolan", help="root folder with images"
         )
-        self.add_argument(
+        parser.add_argument(
             "--save",
             type=bool,
             default=False,
             help="Save the Unet at the end of training. Path is RADOLAN_PATH/Models/RUN-TIMESTAMP"
         )
-        self.add_argument(
+        parser.add_argument(
             "--verbose",
             type=bool,
             default=False,
             help="Verbose setting. Either true or false."
         )
+        return parser
 
 
 class DeprecatedRadolanParser(argparse.ArgumentParser):

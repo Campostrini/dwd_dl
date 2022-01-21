07c49e339a47c2b2b9c4a49e57b0c6e2c7c85fc3
Campostrini <stefano.campostrini@outlook.it>
2022-01-21 12:06:00 +0100

    Fix: Cluster Params

--- dwd_dl/hyperparam_search.py
+++ dwd_dl/hyperparam_search.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     cluster.add_command('source activate py38')
     cluster.add_command('cd $HOME/dwd-dl-thesis/dwd_dl/')
 
-    cluster.per_experiment_nb_cpus = 96
+    cluster.per_experiment_nb_cpus = 3
     cluster.per_experiment_nb_nodes = 1
     cluster.memory_mb_per_node = 96000
 

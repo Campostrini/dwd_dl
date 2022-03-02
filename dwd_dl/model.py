09af8903ad011c126ee57a2e8f1de2262e83d782
Campostrini <stefano.campostrini@outlook.it>
2022-03-02 22:04:50 +0100

    Fix: wrong number of cpus

--- dwd_dl/model.py
+++ dwd_dl/model.py
@@ -303,7 +303,8 @@ class UNetLitModel(pl.LightningModule):
 
     @staticmethod
     def _downsample_block(in_channels, out_channels, name, conv_bias):
-        print(type(in_channels), in_channels, type(out_channels), out_channels)
+        in_channels = int(in_channels)
+        out_channels = int(out_channels)
         return nn.Sequential(
             OrderedDict(
                 [

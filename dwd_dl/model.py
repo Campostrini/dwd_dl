4d2407e31f4829a9f96652bcd4302770889b4702
Campostrini <stefano.campostrini@outlook.it>
2022-03-06 12:34:24 +0100

    Debug

--- dwd_dl/model.py
+++ dwd_dl/model.py
@@ -211,7 +211,6 @@ class UNetLitModel(pl.LightningModule):
         self.apply(self.initialize_weights)
 
         self._loss_weights = torch.tensor(cfg.CFG.WEIGHTS)
-        self._loss_weights = self._loss_weights.to(torch.bfloat16)
         self.loss = torch.nn.CrossEntropyLoss(weight=self._loss_weights)
 
     @staticmethod

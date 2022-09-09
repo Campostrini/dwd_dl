84719fb92c9dd6002c2f983958e3dfc527b6bbb6
Campostrini <stefano.campostrini@outlook.it>
2022-09-09 14:59:00 +0200

    feature: add complete winter ranges

---
+++ dwd_dl/resources/adopt_complete_winter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright (c) 2022 Stefano Campostrini
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+cp COMPLETE_DATE_RANGES_WINTER.yml ~/.radolan_config/DATE_RANGES.yml
+cp COMPLETE_TRAINING_SET_RANGES_WINTER.yml ~/.radolan_config/TRAINING_SET_RANGES.yml
+cp COMPLETE_VALIDATION_SET_RANGES_WINTER.yml ~/.radolan_config/VALIDATION_SET_RANGES.yml
+cp COMPLETE_TEST_SET_RANGES_WINTER.yml ~/.radolan_config/TEST_SET_RANGES.yml
